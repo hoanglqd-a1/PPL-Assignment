1e7fe4dd6685672c069eccfaf9367ad4d4aa3c14
Phuc Hoang <hoanglephuc6@gmail.com>
2025-05-04 22:19:07 +0700

    implement vardecl, constdecl, funcdecl, procdecl

--- extended-assignment/src/main/vm/vm.pl
+++ extended-assignment/src/main/vm/vm.pl
@@ -1,35 +1,93 @@
 %main program
 
 
-reduce_prog([Var,_,Body]) :-
-		create_env(Var,env([[]],false),Env),
-		reduce_stmt(config(Body,Env),_).
+reduce_prog([Var,Func,B]) :-
+		create_env(Var,env([[]],false),Env1),
+		create_env(Func,Env1,Env2),
+		reduce_stmt(config(B,Env2),_).
 											
 %check if X has been declared in the first list of the environment
-has_declared(X,[id(X,Y,Z)|_],id(X,Y,Z)):- !.
-has_declared(X,[_|L],R) :-  has_declared(X,L,R).
+has_declared(X,[id(X,_,_,_)|_],_) :- !.
+has_declared(X,[_|L],R) :- has_declared(X,L,R).
 
 %create a symbol table from the list of variable or constant declarations
 create_env([],L,L).
+
 create_env([var(X,Y)|_],env([_],_),_):- is_builtin(X,_),!,throw(redeclare_identifier(var(X,Y))).
 create_env([var(X,Y)|_],env([L1|_],_),_):-has_declared(X,L1,_),!,throw(redeclare_identifier(var(X,Y))).
-create_env([var(X,Y)|L],env([L1|L2],T),L3):- create_env(L,env([[id(X,var,Y)|L1]|L2],T),L3).
+create_env([var(X,Y)|L],env([L1|L2],T),L3):- create_env(L,env([[id(X,var,Y,nil)|L1]|L2],T),L3).
 
+create_env([const(X,Y)|_],env([_],_),_):- is_builtin(X,_),!,throw(redeclare_identifier(const(X,Y))).
+create_env([const(X,Y)|_],env([L1|_],_),_):-has_declared(X,L1,_),!,throw(redeclare_identifier(const(X,Y))).
+create_env([const(X,Y)|L],env([L1|L2],T),L3):- type_of(Y,Type),create_env(L,env([[id(X,const,Type,Y)|L1]|L2],T),L3).
 
-reduce(config(add(E1,E2),Env),config(R,Env)) :-  
-		reduce_all(config(E1,Env),config(V1,Env)),
-		reduce_all(config(E2,Env),config(V2,Env)),
-		R is V1+V2.
-reduce(config(sub(E1,E2),Env),config(R,Env)) :-  
-		reduce_all(config(E1,Env),config(V1,Env)),
-		reduce_all(config(E2,Env),config(V2,Env)),
-		R is V1-V2.
+create_env([func(I,P,R,B)|_],env([_],_),_):- is_builtin(I,_),!,throw(redeclare_function(func(I,P,R,B))).
+create_env([func(I,P,R,B)|_],env([L1|_],_),_):-has_declared(I,L1,_),!,throw(redeclare_function(func(I,P,R,B))).
+%create_env([func(I,P,R,B)|L],env([L1|L2],T),L3):- create_env(L,env([[id(I,func,[P,R],B)|L1]|L2],T),L3),check_param(P,[]).
+create_env([func(I,P,R,B)|L],env([L1|L2],T),L3):- create_env(L,env([[id(I,func,[P,R],B)|L1]|L2],T),L3),create_env(P,env([[]],false),_).
+
+create_env([proc(I,P,B)|_],env([_],_),_):- is_builtin(I,_),!,throw(redeclare_procedure(proc(I,P,B))).
+create_env([proc(I,P,B)|_],env([L1|_],_),_):-has_declared(I,L1,_),!,throw(redeclare_procedure(proc(I,P,B))).
+create_env([proc(I,P,B)|L],env([L1|L2],T),L3):- create_env(L,env([[id(I,proc,[P|nil],B)|L1]|L2],T),L3),create_env(P,env([[]],false),_).
+
+create_env([par(X,Y)|_],env([L1|_],_),_):- has_declared(X,L1,_),!,throw(redeclare_identifier(par(X,Y))).
+create_env([par(X,Y)|L],env([L1|L2],T),L3):- create_env(L,env([[id(X,par,Y,nil)|L1]|L2],T),L3).
+
+reduce_stmt(config([call(writeInt,[X])],_),_) :-
+		reduce_all(config(X,Env),config(V,Env)),
+		integer(V),!,
+		write(V)
+		.
+
+reduce_stmt(config([call(writeReal,[X])],_),_) :-
+		reduce_all(config(X,Env),config(V,Env)),
+		float(V),!,
+		write(V)
+		.
+
+reduce_stmt(config([call(writeStr,[X])],_),_) :-
+		reduce_all(config(X,Env),config(V,Env)),
+		string(V),!,
+		write(V)
+		.
+
+reduce_stmt(config([call(writeBool,[X])],_),_) :-
+		reduce_all(config(X,Env),config(V,Env)),
+		boolean(V),!,
+		write(V)
+		.
+	
 reduce_all(config(V,Env),config(V,Env)):- integer(V),!.
+reduce_all(config(V,Env),config(V,Env)):- float(V),!.
+reduce_all(config(V,Env),config(V,Env)):- string(V),!.
+reduce_all(config(V,Env),config(V,Env)):- boolean(V),!.
 reduce_all(config(E,Env),config(E2,Env)):-
 		reduce(config(E,Env),config(E1,Env)),!,
 		reduce_all(config(E1,Env),config(E2,Env)).
 
-reduce_stmt(config([call(writeInt,[X])],_),_) :- 
-		reduce_all(config(X,Env),config(V,Env)),
-		write(V)
-		.
+reduce(config(sub(E),Env),config(R,Env)) :- 
+			reduce_all(config(E,Env),config(V,Env)),
+			R is -V.
+reduce(config(add(E1,E2),Env),config(R,Env)) :-
+			reduce_all(config(E1,Env),config(V1,Env)),
+			reduce_all(config(E2,Env),config(V2,Env)),
+			R is V1+V2.
+reduce(config(sub(E1,E2),Env),config(R,Env)) :-
+			reduce_all(config(E1,Env),config(V1,Env)),
+			reduce_all(config(E2,Env),config(V2,Env)),
+			R is V1-V2.
+reduce(config(times(E1,E2),Env),config(R,Env)) :-
+			reduce_all(config(E1,Env),config(V1,Env)),
+			reduce_all(config(E2,Env),config(V2,Env)),
+			R is V1*V2.
+reduce(config(rdiv(E1,E2),Env),config(R,Env)) :-
+			reduce_all(config(E1,Env),config(V1,Env)),
+			reduce_all(config(E2,Env),config(V2,Env)),
+			R is V1/V2.
+
+boolean(true).
+boolean(false).
+type_of(X,integer):- integer(X),!.
+type_of(X,real):- float(X),!.
+type_of(X,string):- string(X),!.
+type_of(X,bool):- boolean(X),!.
--- extended-assignment/src/test/VMSuite.py
+++ extended-assignment/src/test/VMSuite.py
@@ -27,4 +27,64 @@ class VMSuite(unittest.TestCase):
         input = """[[],[],[call(writeInt,[add(4,sub(3,1))])]]."""
         expect = "6"
         self.assertTrue(TestVM.test(input, expect, 405))
+    def test_0(self):
+        input = """[[],[],[call(writeInt,[times(add(1,2),3)])]]."""
+        expect = "9"
+        self.assertTrue(TestVM.test(input, expect, 406))
+    def test_simple_program1(self):        
+        input = """[[],[],[call(writeReal,[3.0])]]."""
+        expect = "3.0"    
+        self.assertTrue(TestVM.test(input, expect, 407))
+    def test_simple_program2(self):
+        input = """[[],[],[call(writeStr,["abc"])]]."""
+        expect = "abc"
+        self.assertTrue(TestVM.test(input, expect, 408))
+    def test_simple_program3(self):
+        input = """[[],[],[call(writeBool,[true])]]."""
+        expect = "true"
+        self.assertTrue(TestVM.test(input, expect, 409))
+    def test_simple_expression0(self):
+        input = """[[],[],[call(writeInt,[times(add(3,1),sub(2))])]]."""
+        expect = "-8"
+        self.assertTrue(TestVM.test(input, expect, 410))
+    def test_simple_expression1(self):
+        input = """[[],[],[call(writeReal,[sub(add(rdiv(5,2),3.6))])]]."""
+        expect = "-6.1"
+        self.assertTrue(TestVM.test(input, expect, 411))
+    def test_constdecl0(self):
+        input = """[[const(a,1)],[],[call(writeInt,[1])]]."""
+        expect = "1"
+        self.assertTrue(TestVM.test(input, expect, 412))
+    def test_constdecl1(self):
+        input = """[[const(a,1),const(a,2)],[],[call(writeInt,[add(a,b)])]]."""
+        expect = "Redeclared identifier: const(a,2)"
+        self.assertTrue(TestVM.test(input, expect, 413))
+    def test_constdecl3(self):
+        input = """[[const(writeInt,1)],[],[call(writeInt,[1])]]."""
+        expect = "Redeclared identifier: const(writeInt,1)"
+        self.assertTrue(TestVM.test(input, expect, 414))
+    def test_funcdecl0(self):
+        input = """[[],[func(foo,[par(a,integer)],integer,[])],[]]."""
+        expect = ""
+        self.assertTrue(TestVM.test(input, expect, 415))
+    def test_funcdecl1(self):
+        input = """[[const(foo,12)],[func(foo,[par(a,integer)],integer,[])],[]]."""
+        expect = "Redeclared function: func(foo,[par(a,integer)],integer,[])"
+        self.assertTrue(TestVM.test(input, expect, 416))
+    def test_funcdecl2(self):
+        input = """[[],[func(foo,[par(a,integer),par(a,float)],integer,[])],[]]."""
+        expect = "Redeclared identifier: par(a,float)"
+        self.assertTrue(TestVM.test(input, expect, 417))
+    def test_procdecl0(self):
+        input = """[[],[proc(foo,[par(a,integer)],[])],[]]."""
+        expect = ""
+        self.assertTrue(TestVM.test(input, expect, 418))
+    def test_procdecl1(self):
+        input = """[[const(foo,12)],[proc(foo,[par(a,integer)],[])],[]]."""
+        expect = "Redeclared procedure: proc(foo,[par(a,integer)],[])"
+        self.assertTrue(TestVM.test(input, expect, 419))
+    def test_procdecl2(self):
+        input = """[[],[proc(foo,[par(a,integer),par(a,float)],[])],[]]."""
+        expect = "Redeclared identifier: par(a,float)"
+        self.assertTrue(TestVM.test(input, expect, 420))
 
